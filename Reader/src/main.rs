e9b5219402a74677117bafd1c5811f7b56b71075
Mårten Ward <Marten.ward@gmail.com>
2024-09-03 23:27:27 +0200

    wip(Reader): read CSV file with the CSV package

--- Reader/src/main.rs
+++ Reader/src/main.rs
@@ -1,3 +1,24 @@
+use std::{error::Error, fs::File, process};
+
+
+fn example() -> Result<(), Box<dyn Error>> {
+    // Build the CSV reader and iterate over each record.
+    let mut file = File::open("../station_data_dataverse.csv")?;
+    let mut rdr = csv::Reader::from_reader(file);
+    for result in rdr.records() {
+        // The iterator yields Result<StringRecord, Error>, so we check the
+        // error here..
+        let record = result?;
+        println!("{:?}", record);
+        return Ok(());
+    }
+    Ok(())
+}
+
 fn main() {
+    if let Err(err) = example() {
+        println!("error running example: {}", err);
+        process::exit(1);
+    }
     println!("Hello, world!");
 }
